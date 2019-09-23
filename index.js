d7540aa1f8f30aa07940f952299f95fd955ac11e
denmercs <denmercs@gmail.com>
2019-09-23 16:03:15 -0500

    fixing some deployment issues

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require("./server");
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log("Server running at port: " + port);
+  console.log(`Listening on ${PORT}`);
 });
