24294ccd17440e5befff8c2897a6ba852a35c371
denmercs <denmercs@gmail.com>
2019-09-23 16:07:59 -0500

    fixing some deployment issues

--- knexfile.js
+++ knexfile.js
@@ -13,53 +13,11 @@ module.exports = {
     },
     seeds: {
       directory: "./database/seed"
-    }
-  },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: "postgresql",
-  //   client: "sqlite3",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
-  production: {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.sqlite3"
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations",
-      tableName: "dbmigrations"
-    },
-    seeds: {
-      directory: "./database/seed"
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
     }
   }
 };
